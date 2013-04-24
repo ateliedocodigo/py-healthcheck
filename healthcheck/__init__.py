6079322f877d5a00c8f7ae17a8b793a0e4e18400
Frank Stratton <epanastasi@epanastasi.com>
2013-04-24 10:51:10 -0700

    adding a basic healthcheck library for flask apps

---
+++ healthcheck/__init__.py
@@ -0,0 +1,90 @@
+import json
+
+def basic_exception_handler(checker, e):
+    return False, str(e)
+
+def json_success_handler(results):
+    data = {
+        'status': 'success',
+        'results': results,
+    }
+
+    return json.dumps(data)
+
+def json_failed_handler(results):
+    data = {
+        'status': 'failure',
+        'results': results,
+    }
+
+    return json.dumps(data)
+
+class HealthCheck(object):
+    def __init__(self, app=None, path=None, success_status=200,
+                 success_headers=None, success_handler=json_success_handler,
+                 failed_status=500, failed_headers=None,
+                 failed_handler=json_failed_handler,
+                 exception_handler=basic_exception_handler, checkers=None,
+                 **options):
+
+        self.app = app
+        self.path = path
+
+        self.success_status = success_status
+        # XXX this default should be somewhere else, maybed returned by the handler
+        self.success_headers = success_headers or {'Content-Type':
+                                                   'application/json'}
+        self.success_handler = success_handler
+
+        self.failed_status = failed_status
+        # XXX this default should be somewhere else, maybed returned by the handler
+        self.failed_headers = failed_headers or {'Content-Type':
+                                                   'application/json'}
+        self.failed_handler = failed_handler
+
+        self.exception_handler = exception_handler
+
+        self.options = options
+        self.checkers = checkers or []
+
+        if self.app and self.path:
+            app.add_url_rule(self.path, view_func=self.check, **options)
+
+    def add_check(self, func):
+        self.checkers.append(func)
+
+    def check(self):
+        results = []
+
+        current_checker = None
+        for checker in self.checkers:
+            current_checker = checker
+            try:
+                passed, output = checker()
+            except Exception as e:
+                passed, output = self.exception_handler(current_checker, e)
+
+            result = {
+                'checker': checker.func_name,
+                'output': output,
+                'passed': passed
+            }
+
+            results.append(result)
+
+        passed = True
+        for result in results:
+            passed = result['passed'] and passed
+
+        if passed:
+            message = "OK"
+            if self.success_handler:
+                message = self.success_handler(results)
+
+            return message, self.success_status, self.success_headers
+        else:
+            message = "NOT OK"
+            if self.failed_handler:
+                message = self.failed_handler(results)
+
+            return message, self.failed_status, self.failed_headers
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='healthcheck',
+      version='0.1.0',
+      description='Adds healthcheck endpoints to Flask apps',
+      author='Frank Stratton',
+      author_email='frank@runscope.com',
+      url='http://runscope.com',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      platforms='any',
+      install_requires=[
+      ],
+)
