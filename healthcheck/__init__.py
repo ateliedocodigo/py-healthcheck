512e400f62eba4482769b1002118c1789aca0823
Ryan Park <ryan@runscope.com>
2014-04-01 12:48:29 -0700

    Add simple mode for healthchecks

--- healthcheck/__init__.py
+++ healthcheck/__init__.py
@@ -1,25 +1,34 @@
 import json
+import socket
 import sys
 import traceback
+
+from flask import request
+
 
 def basic_exception_handler(checker, e):
     return False, str(e)
 
+
 def json_success_handler(results):
     data = {
+        'hostname': socket.gethostname(),
         'status': 'success',
         'results': results,
     }
 
     return json.dumps(data)
 
+
 def json_failed_handler(results):
     data = {
+        'hostname': socket.gethostname(),
         'status': 'failure',
         'results': results,
     }
 
     return json.dumps(data)
+
 
 class HealthCheck(object):
     def __init__(self, app=None, path=None, success_status=200,
@@ -33,15 +42,13 @@ class HealthCheck(object):
         self.path = path
 
         self.success_status = success_status
-        # XXX this default should be somewhere else, maybed returned by the handler
-        self.success_headers = success_headers or {'Content-Type':
-                                                   'application/json'}
+        # XXX this default should be somewhere else, maybe returned by the handler
+        self.success_headers = success_headers or {'Content-Type': 'application/json'}
         self.success_handler = success_handler
 
         self.failed_status = failed_status
-        # XXX this default should be somewhere else, maybed returned by the handler
-        self.failed_headers = failed_headers or {'Content-Type':
-                                                   'application/json'}
+        # XXX this default should be somewhere else, maybe returned by the handler
+        self.failed_headers = failed_headers or {'Content-Type': 'application/json'}
         self.failed_handler = failed_handler
 
         self.exception_handler = exception_handler
@@ -56,34 +63,40 @@ class HealthCheck(object):
         self.checkers.append(func)
 
     def check(self):
+        skip_checks = request.args.get('simple', 'false') == 'true'
+    
         results = []
-
         current_checker = None
         for checker in self.checkers:
             current_checker = checker
-            try:
-                passed, output = checker()
-            except:
-                traceback.print_exc()
-                e = sys.exc_info()[0]
-                self.app.logger.exception(e)
-                passed, output = self.exception_handler(current_checker, e)
+            if skip_checks:
+                result = {
+                    'checker': checker.func_name,
+                    'skipped': True
+                }
+            else:
+                try:
+                    passed, output = checker()
+                except:
+                    traceback.print_exc()
+                    e = sys.exc_info()[0]
+                    self.app.logger.exception(e)
+                    passed, output = self.exception_handler(current_checker, e)
 
-            if not passed:
-                msg = 'Health check "{}" failed with output "{}"'.format(checker.func_name, output)
-                self.app.logger.error(msg)
+                if not passed:
+                    msg = 'Health check "{}" failed with output "{}"'.format(checker.func_name, output)
+                    self.app.logger.error(msg)
 
-            result = {
-                'checker': checker.func_name,
-                'output': output,
-                'passed': passed
-            }
-
+                result = {
+                    'checker': checker.func_name,
+                    'output': output,
+                    'passed': passed
+                }
+                
             results.append(result)
 
-        passed = True
-        for result in results:
-            passed = result['passed'] and passed
+        fn = lambda result, passed: passed and (result.get('passed') or result.get('skipped'))
+        passed = reduce(fn, results)
 
         if passed:
             message = "OK"
