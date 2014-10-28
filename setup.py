c627163a01c82f31f6b8e2522a86c6c7cacbdd0f
Ryan Park <ryan@runscope.com>
2014-10-28 10:56:59 -0700

    Bump version number back to 0.2, and fix spelling of Runscope

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name='healthcheck',
-      version='0.1.1',
+      version='0.2',
       description='Adds healthcheck endpoints to Flask apps',
       author='Frank Stratton',
       author_email='frank@runscope.com',
       url='https://github.com/Runscope/healthcheck',
-      download_url = 'https://github.com/Runccope/healthcheck/tarball/0.1.1',
+      download_url = 'https://github.com/Runscope/healthcheck/tarball/0.2',
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
